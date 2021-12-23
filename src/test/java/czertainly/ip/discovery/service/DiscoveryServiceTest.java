f20b4cee079087675eca0a18757a66b149c40eba
3KeyPradeep <pradeep.saminathan@3key.company>
2021-12-23 13:34:05 +0100

    Update Attribute id to UUID
    Update Attribute id to UUID

--- src/test/java/czertainly/ip/discovery/service/DiscoveryServiceTest.java
+++ src/test/java/czertainly/ip/discovery/service/DiscoveryServiceTest.java
@@ -1,11 +1,8 @@
 package czertainly.ip.discovery.service;
 
-import com.czertainly.api.exception.NotFoundException;
 import com.czertainly.api.model.AttributeDefinition;
 import com.czertainly.api.model.discovery.DiscoveryProviderDto;
 import czertainly.ip.discovery.dao.DiscoveryHistory;
-import czertainly.ip.discovery.repository.CertificateRepository;
-import czertainly.ip.discovery.repository.DiscoveryHistoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +12,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.transaction.Transactional;
-import java.io.IOException;
 import java.util.Arrays;
 
 @SpringBootTest
@@ -37,22 +33,22 @@ public class DiscoveryServiceTest {
         discoveryProviderDtoTest.setConnectorUuid("123456");
 
         AttributeDefinition kind = new AttributeDefinition();
-        kind.setId("72f1ce7d-3e63-458c-8954-2b950240ca33");
+        kind.setUuid("72f1ce7d-3e63-458c-8954-2b950240ca33");
         kind.setName("kind");
         kind.setValue("IP/Hostname");
 
         AttributeDefinition ip = new AttributeDefinition();
-        ip.setId("1b6c48ad-c1c7-4c82-91ef-3b61bc9f52ac");
+        ip.setUuid("1b6c48ad-c1c7-4c82-91ef-3b61bc9f52ac");
         ip.setName("ip");
         ip.setValue("google.com");
 
         AttributeDefinition port = new AttributeDefinition();
-        port.setId("a9091e0d-f9b9-4514-b275-1dd52aa870ec");
+        port.setUuid("a9091e0d-f9b9-4514-b275-1dd52aa870ec");
         port.setName("port");
         port.setValue("443");
 
         AttributeDefinition allPorts = new AttributeDefinition();
-        allPorts.setId("3c70d728-e8c3-40f9-b9b2-5d7256f89ef0");
+        allPorts.setUuid("3c70d728-e8c3-40f9-b9b2-5d7256f89ef0");
         allPorts.setName("allPorts");
         allPorts.setValue("No");
         discoveryProviderDtoTest.setAttributes(Arrays.asList(kind, ip, port, allPorts));
