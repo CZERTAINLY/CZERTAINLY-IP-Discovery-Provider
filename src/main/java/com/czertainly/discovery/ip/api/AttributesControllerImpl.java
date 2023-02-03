7d37c1f68c5c313ee4ef3c1bfa366a6aed6db0d4
3KeyPradeep <pradeep.saminathan@3key.company>
2023-02-03 23:39:12 +0530

    Add request mapping for the attribute API
    Add request mapping for the attribute API

--- src/main/java/com/czertainly/discovery/ip/api/AttributesControllerImpl.java
+++ src/main/java/com/czertainly/discovery/ip/api/AttributesControllerImpl.java
@@ -10,11 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/discoveryProvider/{kind}/attributes")
 public class AttributesControllerImpl implements AttributesController {
 
     private static final Logger logger = LoggerFactory.getLogger(AttributesControllerImpl.class);
